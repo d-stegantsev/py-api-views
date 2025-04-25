d322a120f8680ab838e8f0ad18cb85ec2384e1b0
Dmytro Stehantsev <stegan296@gmail.com>
2025-04-25 19:02:19 +0300

    add patch method to GenreDetail

--- cinema/views.py
+++ cinema/views.py
@@ -40,6 +40,16 @@ class GenreDetail(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def patch(self, request, pk):
+        serializer = GenreSerializer(
+            self.get_object(pk=pk),
+            data=request.data,
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def delete(self, request, pk):
         genre = self.get_object(pk=pk)
         genre.delete()
